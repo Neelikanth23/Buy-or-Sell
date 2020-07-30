3a855a94a2dc4a419ae8c06d5d388f188c20637a
Neelikanth23 <64316630+Neelikanth23@users.noreply.github.com>
2020-07-31 00:16:47 +0530

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(() => ({
+  typographyStyles: {
+    flex: 1
+  }
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography className={classes.typographyStyles}>Neelikanth</Typography>
+        <AddAPhotoIcon />
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
